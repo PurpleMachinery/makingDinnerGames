832f4ef4ce96abac06d552bb892a9ff40b6651ee
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2018-08-30 11:01:29 -0300

    added white frame and hit accuracy in the size of the Oval

--- src/JogoCirculo/JogoCirculo.java
+++ src/JogoCirculo/JogoCirculo.java
@@ -1,9 +1,11 @@
 package JogoCirculo;
 
-import jdk.nashorn.internal.ir.WhileNode;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 
 public class JogoCirculo extends Engine.Game {
 
@@ -16,8 +18,8 @@ public class JogoCirculo extends Engine.Game {
     public void onLoad() {
         posX = 0;
         posY = 0;
-        accX = 10;
-        accY = 10;
+        accX = 2;
+        accY = 2;
     }
 
     @Override
@@ -34,6 +36,10 @@ public class JogoCirculo extends Engine.Game {
         }
         if (posY < 0 || posY > getHeight()) {
             accY *= -1;
+        } else if (accY > 0 && posY > getHeight() - 20) {
+            accY *= -1;
+        } else if (accX > 0 && posX > getWidth() - 20) {
+            accX *= -1;
         }
     }
 
@@ -41,5 +47,14 @@ public class JogoCirculo extends Engine.Game {
     public void onRender(Graphics2D g) {
         g.setColor(Color.white);
         g.fillOval(posX, posY, 20, 20);
+
+        //TESTE
+        Shape r = new Rectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1);
+        Shape c = new Ellipse2D.Float(0, 0, getWidth() - 1, getHeight() - 1);
+        Shape l = new Line2D.Float(100, 100, getWidth() / 2, getHeight() / 2);
+
+        g.draw(r);
+
+
     }
 }
